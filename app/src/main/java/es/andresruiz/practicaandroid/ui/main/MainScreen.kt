2386543ff323a6f2283610b4fcaae65adfbd2bef
Andrés Ruiz <andresruizsan12@gmail.com>
2025-04-11 19:27:30 +0200

    Deleted unnecessary imports

--- app/src/main/java/es/andresruiz/practicaandroid/ui/main/MainScreen.kt
+++ app/src/main/java/es/andresruiz/practicaandroid/ui/main/MainScreen.kt
@@ -9,10 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

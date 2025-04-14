aae8c0c7b798a6fbd3aa676164b6a607586d3708
Andrés Ruiz <andresruizsan12@gmail.com>
2025-04-14 14:04:25 +0200

    Added retrofit dependencies

--- data-retrofit/build.gradle.kts
+++ data-retrofit/build.gradle.kts
@@ -37,6 +37,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

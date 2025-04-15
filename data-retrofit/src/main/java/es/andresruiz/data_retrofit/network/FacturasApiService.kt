244401dbc5766e65358470a50500cf6e743b676a
Andrés Ruiz <andresruizsan12@gmail.com>
2025-04-15 18:39:33 +0200

    Used @MockCircular to have different responses each time

--- data-retrofit/src/main/java/es/andresruiz/data_retrofit/network/FacturasApiService.kt
+++ data-retrofit/src/main/java/es/andresruiz/data_retrofit/network/FacturasApiService.kt
@@ -1,14 +1,21 @@
 package es.andresruiz.data_retrofit.network
 
 import co.infinum.retromock.meta.Mock
+import co.infinum.retromock.meta.MockCircular
 import co.infinum.retromock.meta.MockResponse
+import co.infinum.retromock.meta.MockResponses
 import es.andresruiz.domain.models.FacturasResponse
 import retrofit2.http.GET
 
 interface FacturasApiService {
 
     @Mock
-    @MockResponse(body = "mock_facturas.json")
+    @MockCircular
+    @MockResponses(
+        MockResponse(body = "mock_facturas_no_pagadas.json"),
+        MockResponse(body = "mock_facturas.json"),
+        MockResponse(body = "mock_facturas_pagadas.json")
+    )
     @GET("facturas")
     suspend fun getFacturas(): FacturasResponse
 }

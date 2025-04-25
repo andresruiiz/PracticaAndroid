845bc8b57aa4582dae6fb1ef7a92f96aaaf0b4fd
Andrés Ruiz <andresruizsan12@gmail.com>
2025-04-25 13:54:12 +0200

    Added junit and mockito dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
 
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))

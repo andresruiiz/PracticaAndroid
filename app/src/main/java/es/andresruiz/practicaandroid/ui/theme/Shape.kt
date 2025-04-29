82815b06a40f6b3d941ea434c99e101ee567f426
Andrés Ruiz <andresruizsan12@gmail.com>
2025-04-29 12:01:34 +0200

    Deleted unnecessary imports

--- app/src/main/java/es/andresruiz/practicaandroid/ui/theme/Shape.kt
+++ app/src/main/java/es/andresruiz/practicaandroid/ui/theme/Shape.kt
@@ -1,7 +1,5 @@
 package es.andresruiz.practicaandroid.ui.theme
 
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.graphics.RectangleShape
